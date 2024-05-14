4b3ff9967af6847c621a407585cf503dfb58db0b
Susobhan Dash <susobhan.dash@gmail.com>
2024-05-14 21:37:10 +0530

    Style upates
    Added Expo Poppins Font, Expo Material Theme and React Native Paper

--- App.jsx
+++ App.jsx
@@ -1,27 +1,176 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 import Routes from './src/Routes/Routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './src/Redux/Store';
 
+import {
+    MD3DarkTheme,
+    MD3LightTheme,
+    PaperProvider,
+    configureFonts,
+} from 'react-native-paper';
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
+import {
+    useFonts,
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
+} from '@expo-google-fonts/poppins';
+
 export default function App() {
-  return (
+    const colorScheme = useColorScheme();
+    const sourceColor = colorScheme === 'dark' ? '#ff6995' : '#ec407a';
 
-    <Provider store={store} >
-     <NavigationContainer>
-      <Routes/>
-     </NavigationContainer>
-     </Provider>
-  
-  );
+    let [loaded] = useFonts({
+        Poppins_100Thin,
+        Poppins_100Thin_Italic,
+        Poppins_200ExtraLight,
+        Poppins_200ExtraLight_Italic,
+        Poppins_300Light,
+        Poppins_300Light_Italic,
+        Poppins_400Regular,
+        Poppins_400Regular_Italic,
+        Poppins_500Medium,
+        Poppins_500Medium_Italic,
+        Poppins_600SemiBold,
+        Poppins_600SemiBold_Italic,
+        Poppins_700Bold,
+        Poppins_700Bold_Italic,
+        Poppins_800ExtraBold,
+        Poppins_800ExtraBold_Italic,
+        Poppins_900Black,
+        Poppins_900Black_Italic,
+    });
+
+    const baseFont = {
+        fontFamily: 'Mono-Sans',
+    };
+
+    const baseVariants = configureFonts({ config: baseFont });
+    const customVariants = {
+        bodyLarge: {
+            ...baseVariants.bodyLarge,
+            fontFamily: 'Poppins_300Light',
+        },
+        bodyMedium: {
+            ...baseVariants.bodyMedium,
+            fontFamily: 'Poppins_400Regular',
+        },
+        bodySmall: {
+            ...baseVariants.bodySmall,
+            fontFamily: 'Poppins_300Light',
+        },
+
+        displayLarge: {
+            ...baseVariants.displayLarge,
+            fontFamily: 'Poppins_300Light',
+        },
+        displayMedium: {
+            ...baseVariants.displayMedium,
+            fontFamily: 'Poppins_400Regular',
+        },
+        displaySmall: {
+            ...baseVariants.displaySmall,
+            fontFamily: 'Poppins_300Light',
+        },
+
+        headlineLarge: {
+            ...baseVariants.headlineLarge,
+            fontFamily: 'Poppins_600SemiBold',
+        },
+        headlineMedium: {
+            ...baseVariants.headlineMedium,
+            fontFamily: 'Poppins_600SemiBold',
+        },
+        headlineSmall: {
+            ...baseVariants.headlineSmall,
+            fontFamily: 'Poppins_500Medium',
+        },
+
+        labelLarge: {
+            ...baseVariants.labelLarge,
+            fontFamily: 'Poppins_400Regular',
+        },
+        labelMedium: {
+            ...baseVariants.labelMedium,
+            fontFamily: 'Poppins_300Light',
+        },
+        labelSmall: {
+            ...baseVariants.labelSmall,
+            fontFamily: 'Poppins_200ExtraLight',
+        },
+
+        titleLarge: {
+            ...baseVariants.titleLarge,
+            fontFamily: 'Poppins_500Medium',
+        },
+        titleMedium: {
+            ...baseVariants.titleMedium,
+            fontFamily: 'Poppins_600SemiBold',
+        },
+        titleSmall: {
+            ...baseVariants.titleSmall,
+            fontFamily: 'Poppins_600SemiBold',
+        },
+    };
+
+    const fonts = configureFonts({
+        config: {
+            ...baseVariants,
+            ...customVariants,
+        },
+    });
+
+    const { theme } = useMaterial3Theme();
+
+    const paperTheme =
+        colorScheme === 'dark'
+            ? {
+                  ...MD3DarkTheme,
+                  colors: theme.dark,
+                  fonts: fonts,
+              }
+            : {
+                  ...MD3LightTheme,
+                  colors: theme.light,
+                  fonts: fonts,
+              };
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: paperTheme.colors.elevation.level1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Routes theme={paperTheme} />
+            </NavigationContainer>
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,99 +1,112 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Poster from '../Component/Poster'
-import CardsTitle from '../Component/CardsTitle'
-import Cards from '../Component/Cards'
-import Header from '../Component/Header'
-import { LinearGradient } from 'expo-linear-gradient'
-import { getNowPlayingMovies } from '../Network/Network'
-import { getPopularMovies } from '../Network/Network'
-import Search from '../Component/Search'
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Poster from '../Component/Poster';
+import CardsTitle from '../Component/CardsTitle';
+import Cards from '../Component/Cards';
+import Header from '../Component/Header';
+import { LinearGradient } from 'expo-linear-gradient';
+import { getNowPlayingMovies } from '../Network/Network';
+import { getPopularMovies } from '../Network/Network';
+import Search from '../Component/Search';
+import { withTheme } from 'react-native-paper';
 
-const Home = ({navigation}) => {
-  const [nowPlayingData, setNowPlayingData] = useState([]);
-  const [popularMoviesData, setPopularMoviesData] = useState([]);
-  // const [topRatedMoviesData, setTopRatedMoviesData] = useState([]);
-  // const [upComingMoviesData, setUpComingMoviesData] = useState([]);
+const Home = ({ navigation, theme }) => {
+    const [nowPlayingData, setNowPlayingData] = useState([]);
+    const [popularMoviesData, setPopularMoviesData] = useState([]);
+    // const [topRatedMoviesData, setTopRatedMoviesData] = useState([]);
+    // const [upComingMoviesData, setUpComingMoviesData] = useState([]);
 
-  useEffect(() => {
-    const handleApi = async () => {
-      try {
-        const { data, status } = await getNowPlayingMovies();
-        if (status === 200) {
-          setNowPlayingData(data.results);
-        } else {
-          Alert.alert(`Request failed with ${data}`);
-        }
-      } catch (error) {
-        console.log(error);
-        Alert.alert(`Request failed with error: ${error.message}`);
-      }
-    };
+    useEffect(() => {
+        const handleApi = async () => {
+            try {
+                const { data, status } = await getNowPlayingMovies();
+                if (status === 200) {
+                    setNowPlayingData(data.results);
+                } else {
+                    Alert.alert(`Request failed with ${data}`);
+                }
+            } catch (error) {
+                console.log(error);
+                Alert.alert(`Request failed with error: ${error.message}`);
+            }
+        };
 
-    handleApi();
-  }, [])
+        handleApi();
+    }, []);
 
-  useEffect(() => {
-    const handleApi = async () => {
-      try {
-        const { data, status } = await getPopularMovies();
-        if (status === 200) {
-          setPopularMoviesData(data.results);
-        } else {
-          Alert.alert(`Request failed with ${data}`);
-        }
-      } catch (error) {
-        console.log(error);
-        Alert.alert(`Request failed with error: ${error.message}`);
-      }
-    };
+    useEffect(() => {
+        const handleApi = async () => {
+            try {
+                const { data, status } = await getPopularMovies();
+                if (status === 200) {
+                    setPopularMoviesData(data.results);
+                } else {
+                    Alert.alert(`Request failed with ${data}`);
+                }
+            } catch (error) {
+                console.log(error);
+                Alert.alert(`Request failed with error: ${error.message}`);
+            }
+        };
 
-    handleApi();
-  })
+        handleApi();
+    });
 
-  // useEffect(() => {
-  //   const handleApi = async () => {
-  //     try {
-  //       const { data, status } = await getTopRatedMovies();
-  //       if (status === 200) {
-  //         setTopRatedMoviesData(data.results);
-  //       } else {
-  //         Alert.alert(`Request failed with ${data}`);
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //       Alert.alert(`Request failed with error: ${error.message}`);
-  //     }
-  //   };
+    // useEffect(() => {
+    //   const handleApi = async () => {
+    //     try {
+    //       const { data, status } = await getTopRatedMovies();
+    //       if (status === 200) {
+    //         setTopRatedMoviesData(data.results);
+    //       } else {
+    //         Alert.alert(`Request failed with ${data}`);
+    //       }
+    //     } catch (error) {
+    //       console.log(error);
+    //       Alert.alert(`Request failed with error: ${error.message}`);
+    //     }
+    //   };
 
-  //   handleApi();
-  // })
+    //   handleApi();
+    // })
 
+    return (
+        // VirtualizedLists should never be nested inside plain ScrollViews with the same orientation
+        // because it can break windowing and other functionality - use another VirtualizedList-backed container instead.
+        // Basically you can't nest flatList{VirtualisedLists} inside scrollView so put scrollView top then safeAreaView
+        // <ScrollView>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Header />
+                {/* <Text>Hello</Text> */}
+                <Search navigation={navigation} />
 
-  return (
-    // VirtualizedLists should never be nested inside plain ScrollViews with the same orientation 
-    // because it can break windowing and other functionality - use another VirtualizedList-backed container instead.
-    // Basically you can't nest flatList{VirtualisedLists} inside scrollView so put scrollView top then safeAreaView
-    <ScrollView > 
-    <SafeAreaView style={{flex:1 }}>
-        <Header/>
-      {/* <Text>Hello</Text> */}
-       <Search navigation={navigation}/>
-        <Poster />
+                <ScrollView>
+                    <Poster />
 
-      <CardsTitle name ="Trending Movies" />
-      
-      <Cards navigation={navigation} data={nowPlayingData} />
-      <CardsTitle name ="Latest Movies"  />
-      <Cards navigation={navigation} data={popularMoviesData} />
-     
-    </SafeAreaView>
-    </ScrollView>
-  )
-}
+                    <View style={styles.bodyContent}>
+                        <CardsTitle name='Trending Movies' />
 
-export default Home
+                        <Cards
+                            navigation={navigation}
+                            data={nowPlayingData}
+                        />
+                        <CardsTitle name='Latest Movies' />
+                        <Cards
+                            navigation={navigation}
+                            data={popularMoviesData}
+                        />
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        // </ScrollView>
+    );
+};
 
-const styles = StyleSheet.create({})
+export default withTheme(Home);
 
+const styles = StyleSheet.create({
+    bodyContent: {
+        paddingVertical: 15,
+    },
+});
